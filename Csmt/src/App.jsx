89cc242cc86f307973ecbcb9d4a29118ae9542a3
Azam0110 <azamhusain0987@gmail.com>
2024-09-27 17:21:04 +0545

    Cmst clone
    Features Page of CMST clone website

---
+++ Csmt/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import NavBar from './components/Header'
+import Features from './components/Features'
+import Component from './components/Footer'
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBar/>
+      <Features/>
+      <Component/>
+    </div>
+  )
+}
+
+export default App
---
+++ Csmt/src/components/Features.jsx
@@ -0,0 +1,230 @@
+import React from 'react'
+
+function Features() {
+    return (
+        <div>
+
+            <section class="py-12">
+                <div class="container mx-auto">
+                    <article class="text-center max-w-3xl mx-auto">
+                        <h1 class="text-black font-bold text-[45px]">
+                            <span class="text-shadow: 0px 4px 3px rgba(34, 197, 94, 1);">15+ Features</span> On The Go!
+                        </h1>
+                    </article>
+                    
+                    <div class="flex flex-wrap justify-center mt-8 gap-6">
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">file_copy</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Applicant Form & Records System</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Keep track of all your applicants on all steps of their processing. With the intuitive profile created for each applicant, you can easily maintain their records about classes, VISA, tests, documents, payments, and many more in a single place.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">attach_money</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Commissions Management</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Easily manage commissions of all your applicants with the click of a button. Track commission collection times with reminders and emails.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">list_alt</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Commission Invoice Generation</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Avoid the hassle of manually creating invoices for each commission with the innovative invoice generation. You can directly send the automatically generated invoice to the receiver from within the system.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">list_alt</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Classes Management</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Do you run training, language, and test preparation classes? Now keep track of all of them and their enrolled students. You can also manage attendance of your students.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">list_alt</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Payments Management and History</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Easily manage records of the payments of each individual applicant.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">sms</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Send Bulk SMS with your Identity</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Send SMS to many applicants all at once. You have your own identity (Display name in SMS) so that the receivers recognize you instantly.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">email</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Send Bulk Emails</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Send emails to many applicants from within CMST with your designated email address.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">list_alt</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Courses, University/College, and Country Management</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Store details of all your colleges, universities, and their courses for use in the system. These details can be used in applicant's processing, mobile apps, and in Consultancy Finder.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">dashboard</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Organize and Categorize your Information</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">With the intuitive Tags feature, you can group different applicants and assign them tags for bulk actions.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">timeline</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Enable CMST in your Branch Offices</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Create accounts for your branch offices directly from CMST. The branch offices will have their own space to work in, and you can monitor their activity from the main account.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">speaker_notes</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Tasks with To-do List</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Create and assign tasks for efficient productivity. You can create to-do checklists to list out specific task details and discuss them in the comment section.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">chat</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Real-time Chatting and Messaging</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Chat with each other in real-time from within CMST.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">verified_user</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">User-based, Role-based Login</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Give necessary access to users by assigning them roles and permissions. Secure your data by managing who can or cannot see information in the system.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">assignment</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Automatic User Daily Report</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Receive an automated report of your users' activity in your email so you can review their overall daily work.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">receipt</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Online IELTS Booking</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">Book test dates for IELTS directly from CMST.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full md:w-5/12 lg:w-5/12">
+                            <div class="rounded-lg p-2">
+                                <figure class="bg-gray-200 rounded-full h-[55px] w-[55px] inline-flex items-center justify-end">
+                                    <span class="material-icons text-green-700 text-4xl">mobile_friendly</span>
+                                </figure>
+                                <div class="mt-4">
+                                    <h5 class="text-green-600 text-[23px] font-medium">Integration with your Customized Mobile App</h5>
+                                    <p class="mt-2 text-gray-700 text-[18px]">CMST can be fully integrated with your customized mobile app (for both Android & iOS) so that applicants can easily stay up-to-date with their processing and other details.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="relative bg-green-600">
+                <div class="container mx-auto">
+                    <img class="w-full object-contain" src="https://cmst.xyz/css/v2/image/start_using_cmst.png" alt="cmst image"></img>
+                    <article class="absolute inset-0 flex flex-col items-center justify-center text-white text-center">
+                        <h2 class="text-white font-semibold text-[38px] uppercase">Start using CMST Today</h2>
+                        <p class="mt-2">Only a few more steps to start your digital revolution.</p>
+                        <a href="https://cmst.xyz/contact" class="mt-4 bg-white text-green-400 text-[18px] font-medium py-2 px-6 rounded-full capitalize hover:bg-gray-100 hover:text-black transition duration-300">Contact</a>
+                    </article>
+                </div>
+            </section>
+
+
+
+        </div>
+
+    )
+}
+
+export default Features
